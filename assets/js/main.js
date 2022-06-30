23c3b2ba59d3947565c43445c8c793f20d615e40
Steve Fenton <99181436+steve-fenton-octopus@users.noreply.github.com>
2022-06-30 10:49:20 +0100

    Remove jQuery.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,59 @@
-$(function () {
+(function () {
   // Cache variables for increased performance on devices with slow CPUs.
-  var flexContainer = $('div.flex-container')
-  var searchBox = $('.search-box')
-  var searchClose = $('.search-icon-close')
-  var searchInput = $('#search-input')
+  var flexContainer = document.querySelector('div.flex-container');
+  var searchIcon = document.querySelector('.search-icon');
+  var searchBox = document.querySelector('.search-box');
+  var searchClose = document.querySelector('.search-icon-close');
+  var searchInput = document.querySelector('#search-input');
+  var activeClass = 'active';
+  var searchActiveClass = 'search-active';
+
+  // If elements not present, no need to continue
+  if (!searchIcon) {
+    return;
+  }
+
+  function toggle(elem, className) {
+    elem.classList.contains(className)
+      ? elem.classList.remove(className)
+      : elem.classList.add(className);
+  }
 
   // Menu Settings
-  $('.menu-icon, .menu-icon-close').click(function (e) {
-    e.preventDefault()
-    e.stopPropagation()
-    flexContainer.toggleClass('active')
-  })
+  document.querySelectorAll('.menu-icon, .menu-icon-close').forEach(function (elem) {
+    elem.addEventListener('click', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      toggle(flexContainer, activeClass);
+    });
+  });
 
   // Click outside of menu to close it
-  flexContainer.click(function (e) {
-    if (flexContainer.hasClass('active') && e.target.tagName !== 'A') {
-      flexContainer.removeClass('active')
+  flexContainer.addEventListener('click', function (e) {
+    if (flexContainer.classList.contains(activeClass) && e.target.tagName !== 'A') {
+      flexContainer.classList.remove(activeClass)
     }
-  })
+  });
 
   // Press Escape key to close menu
-  $(window).keydown(function (e) {
+  document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape') {
-      if (flexContainer.hasClass('active')) {
-        flexContainer.removeClass('active')
-      } else if (searchBox.hasClass('search-active')) {
-        searchBox.removeClass('search-active')
-      }
+      flexContainer.classList.remove(activeClass);
+      searchBox.classList.remove(searchActiveClass);
     }
-  })
+  });
 
   // Search Settings
-  $('.search-icon').click(function (e) {
-    e.preventDefault()
-    searchBox.toggleClass('search-active')
-    searchInput.focus()
+  searchIcon.addEventListener('click', function (e) {
+    e.preventDefault();
+    toggle(searchBox, searchActiveClass);
+    searchInput.focus();
 
-    if (searchBox.hasClass('search-active')) {
-      searchClose.click(function (e) {
-    		e.preventDefault()
-    		searchBox.removeClass('search-active')
-    	})
+    if (searchBox.classList.contains(searchActiveClass)) {
+      searchClose.addEventListener('click', function (e) {
+    		e.preventDefault();
+    		searchBox.classList.remove(searchActiveClass);
+    	});
     }
-  })
-})
+  });
+})();
